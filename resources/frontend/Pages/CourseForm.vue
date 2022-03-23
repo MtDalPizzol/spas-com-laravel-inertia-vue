a79022c7501b5e246007c14750cb7c0d448588f7
Matheus Dal'Pizzol <matheusdalpizzol@gmail.com>
2022-03-23 12:42:07 +0000

    Criando o formulario para novos cursos

--- resources/frontend/Pages/CourseForm.vue
+++ resources/frontend/Pages/CourseForm.vue
@@ -1,7 +1,45 @@
 <template>
   <Head :title="title" />
   <Authenticated>
-    <h1>{{ title }}</h1>
+    <div class="row q-col-gutter-md">
+      <div class="col-12 col-sm-8 tw-flex-col tw-space-y-4">
+        <q-input
+          v-model="form.title"
+          outlined
+          label="Título"
+        />
+
+        <q-editor
+          v-model="form.description"
+          placeholder="Descrição"
+        />
+      </div>
+      <div class="col-12 col-sm-4 tw-flex-col tw-space-y-4">
+        <q-file
+          v-model="form.cover"
+          outlined
+          clearable
+          label="Capa"
+          @update:model-value="preview"
+        >
+          <template #prepend>
+            <i-fa-solid-camera-retro />
+          </template>
+        </q-file>
+
+        <div class="tw-border tw-p-2 tw-rounded tw-w-full tw-h-max tw-flex tw-justify-center tw-items-center">
+          <img
+            v-if="coverUrl"
+            :src="coverUrl"
+            alt="Capa do curso"
+          >
+          <i-fa-solid-camera-retro
+            v-else
+            class="tw-text-6xl tw-text-gray-200"
+          />
+        </div>
+      </div>
+    </div>
   </Authenticated>
 </template>
 
@@ -9,4 +47,21 @@
 defineProps({
   title: String
 })
+
+const form = reactive({
+  title: null,
+  description: '',
+  cover: null
+})
+
+const coverUrl = ref(null)
+
+const preview = (file) => {
+  if (!form.cover) {
+    coverUrl.value = null
+    return
+  }
+
+  coverUrl.value = URL.createObjectURL(form.cover)
+}
 </script>
